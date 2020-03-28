32740190e4b1ca12628fd70cce58479ffac6d6dc
Sérgio Santos <me@s3rgiosan.com>
2020-03-28 15:41:04 +0000

    Hide product sale flash text

--- lib/Frontend.php
+++ lib/Frontend.php
@@ -43,7 +43,9 @@ class Frontend {
 		\remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart', 10 );
 		\remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 10 );
 		\remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30 );
-		\remove_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_show_product_loop_sale_flash', 6 );
+		\remove_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_show_product_loop_sale_flash', 10 );
+		\remove_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_show_product_loop_sale_flash', 10 );
+		\remove_action( 'woocommerce_before_single_product_summary', 'woocommerce_show_product_sale_flash', 10 );
 
 		\add_filter( 'woocommerce_loop_add_to_cart_link', '__return_empty_string', 10 );
 		\add_filter( 'woocommerce_is_purchasable', '__return_false' );
